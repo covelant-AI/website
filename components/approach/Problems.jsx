4d35b69f8615504da2d12eef331a6d174f301e7c
AmNat789 <amit.amanna@code.berlin>
2025-04-29 18:46:32 +0200

    Remove commented-out code in Problems component to clean up the file

--- components/approach/Problems.jsx
+++ components/approach/Problems.jsx
@@ -76,24 +76,3 @@ export default function Problems() {
         </div>
     );
 }
-
-
-// const t = useTranslations('components.approach.intro');
-// return (
-//     <div className="relative">
-//       <section
-//         className="flex flex-col items-center justify-center z-1"
-//       >
-//         <div className="mt-[182px] flex flex-col items-center">
-//             <h1 className="text-[80px] font-bold leading-120% tracking-[-2%] bg-gradient-to-r from-[#000000] to-[#B3C0DF] text-transparent bg-clip-text">{t('title')}</h1>
-//             <p className="w-[600px] text-[24px] font-normal leading-[120%] tracking-[-2%] text-[#000000] opacity-80">
-//                 {t('description')}
-//             </p>
-//         </div>
-//         <Image className="mask-b-from-20% mask-b-to-110%" src="/images/macbookMockup1.png" alt="mackbook mockup 1" width={1021} height={662} />
-      
-//         <RadialBlurBg width={"150vw"} height={"150vh"} rotate={"-9.35deg"} top={"-50vh"} left={"-25vw"}/>
-//       </section>
-//     </div>
-//   );
-// }
