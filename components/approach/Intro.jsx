632f2e43c5bc721117d714818c2f053b4e469900
AmNat789 <amit.amanna@code.berlin>
2025-04-29 18:44:03 +0200

    Format using Breakpoitns

--- components/approach/Intro.jsx
+++ components/approach/Intro.jsx
@@ -1,16 +1,17 @@
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import RadialBlurBg from '@components/RadialBlurBg';
+
 export default function ApproachIntro() {
     const t = useTranslations('components.approach.intro');
     return (
     <div className="relative">
       <section
-        className="flex flex-col items-center justify-center z-1"
+        className=" z-1"
       >
         <div className="mt-[182px] flex flex-col items-center">
-            <h1 className="text-[80px] font-bold leading-120% tracking-[-2%] bg-gradient-to-r from-[#000000] to-[#B3C0DF] text-transparent bg-clip-text">{t('title')}</h1>
-            <p className="w-[600px] text-[24px] font-normal leading-[120%] tracking-[-2%] text-[#000000] opacity-80">
+            <h1 className="text-[36px] lg:text-[80px] font-bold leading-120% tracking-[-2%] bg-gradient-to-r from-[#000000] to-[#B3C0DF] text-transparent bg-clip-text">{t('title')}</h1>
+            <p className="w-[300px] lg:w-[600px] text-center text-[16px] xl:text-[24px] font-normal leading-[120%] tracking-[-2%] text-[#000000] opacity-80">
                 {t('description')}
             </p>
         </div>
