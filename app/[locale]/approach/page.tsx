6c6f2f29449a370da5a5427df18b4678b489f3a3
AmNat789 <amit.amanna@code.berlin>
2025-04-29 19:02:35 +0200

    impliment existing components

--- app/[locale]/approach/page.tsx
+++ app/[locale]/approach/page.tsx
@@ -1,11 +1,14 @@
 import Intro from "@components/approach/Intro";
 import Problems from "@components/approach/Problems";
-
+import InfiniteCarousel from "@components/InfiniteCarousel";
+import ScalingCards from "@components/ScalingCards";
 export default function Approach() {
     return (
-        <div className="bg-[#F9F9F9] flex flex-col items-center justify-center z-1 gap-y-[80px] lg:gap-y-[160px] px-[10%]">
+        <div className="bg-[#F9F9F9] flex flex-col items-center justify-center z-1 gap-y-[80px] lg:gap-y-[160px] px-[10%] overflow-x-hidden">
             <Intro />
             <Problems />
+            <InfiniteCarousel/>
+            <ScalingCards/>
         </div>   
     )
 }
