612aeee940cfe65b340f6a812e29381d4aad1395
AmNat789 <amit.amanna@code.berlin>
2025-04-29 19:53:46 +0200

    feat: add Solution component to Approach page and update localization request

--- app/[locale]/approach/page.tsx
+++ app/[locale]/approach/page.tsx
@@ -2,11 +2,14 @@ import Intro from "@components/approach/Intro";
 import Problems from "@components/approach/Problems";
 import InfiniteCarousel from "@components/InfiniteCarousel";
 import ScalingCards from "@components/ScalingCards";
+import Solution from "@components/approach/Solution";
+
 export default function Approach() {
     return (
         <div className="bg-[#F9F9F9] flex flex-col items-center justify-center z-1 gap-y-[80px] lg:gap-y-[160px] px-[10%] overflow-x-hidden">
             <Intro />
             <Problems />
+            <Solution/>
             <InfiniteCarousel/>
             <ScalingCards/>
         </div>   
