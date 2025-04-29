764e2b4e36682991c0b0c4880db36b7aaf66249e
AmNat789 <amit.amanna@code.berlin>
2025-04-29 17:59:13 +0200

    feat: add Problems component and integrate into Approach page with translations

--- components/approach/Intro.jsx
+++ components/approach/Intro.jsx
@@ -1,10 +1,10 @@
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
-
+import RadialBlurBg from '@components/RadialBlurBg';
 export default function ApproachIntro() {
     const t = useTranslations('components.approach.intro');
     return (
-    <div className="relative bg-[#F9F9F9]">
+    <div className="relative">
       <section
         className="flex flex-col items-center justify-center z-1"
       >
@@ -16,13 +16,7 @@ export default function ApproachIntro() {
         </div>
         <Image className="mask-b-from-20% mask-b-to-110%" src="/images/macbookMockup1.png" alt="mackbook mockup 1" width={1021} height={662} />
       
-        {/* Background gradient */}
-        <div 
-          className="absolute top-[-50vh] left-[-25vw] w-[150vw] h-[150vh] rotate-[-9.35deg] z-0"
-          style={{
-            background: 'radial-gradient(50% 50% at 50% 50%, rgba(8, 113, 151, 0.04) 0%, rgba(0, 180, 173, 0.12) 50%, rgba(176, 199, 255, 0.2) 80%, rgba(176, 199, 255, 0) 100%)'
-        }}
-        />
+        <RadialBlurBg width={"150vw"} height={"150vh"} rotate={"-9.35deg"} top={"-50vh"} left={"-25vw"}/>
       </section>
     </div>
   );
---
+++ components/approach/Problems.jsx
@@ -0,0 +1,95 @@
+import { useTranslations } from 'next-intl';
+
+export default function Problems() {
+    const t = useTranslations('components.approach.problems');
+
+    const problems = [
+        {
+            text: t('c1'),
+            icon: '/images/timer.svg',
+        },
+        {
+            text: t('c2'),
+            icon: '/images/exclamationMark.svg',
+        },
+        {
+            text: t('c3'),
+            icon: '/images/growthIcon.svg',
+        },
+        {
+            text: t('c4'),
+            icon: '/images/lock.svg',
+        },
+        {
+            text: t('c5'),
+            icon: '/images/money.svg',
+        },
+        {
+            text: t('c6'),
+            icon: '/images/settings.svg',
+        },
+    ]
+
+    return (
+        <div className="relative">
+            <section>
+            <h1 className="text-[80px] font-semibold leading-100% tracking-[-2%] text-[#000000]"
+            >{t('title')}</h1>
+
+            <div className="relative grid grid-cols-2 xl:grid-cols-3 gap-[25px]">
+                {problems.map((problem, index) => {
+                    let direction1 = 'xl:bg-linear-to-b'
+                    let direction2 = 'bg-linear-to-r'
+                    
+                    if(index >= 3){
+                        direction1 = 'xl:bg-linear-to-t'
+                    }
+                    if(index % 2 != 0) {
+                        direction2 = 'bg-linear-to-l'
+                    }
+
+                    let className = [direction1, direction2, "from-[#27283E] to-[#A53942] flex flex-col gap-[71px] pl-[28px] pr-[65px] pt-[24px] pb-[36px] rounded-[24px] z-10"].join(' ')
+
+                    return (
+                        <div
+                        key={index}
+                        className={className}
+                    >
+                        <div className="block relative h-[64px] w-[64px]">
+                            <img className="h-[64px] w-[64px]" src={"/images/hexagonRed.svg"} alt={`icon-${index}`} />
+                            <img className="h-[34px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" src={problem.icon} alt={`icon-${index}`} />
+                        </div>
+
+                        <p className="font-normal text-[24px] tracking-[-2%] text-[#FFFFFF]">{problem.text}</p>
+                    </div>
+                    )
+                }
+                )}
+            </div>
+
+
+            </section>
+        </div>
+    );
+}
+
+
+// const t = useTranslations('components.approach.intro');
+// return (
+//     <div className="relative">
+//       <section
+//         className="flex flex-col items-center justify-center z-1"
+//       >
+//         <div className="mt-[182px] flex flex-col items-center">
+//             <h1 className="text-[80px] font-bold leading-120% tracking-[-2%] bg-gradient-to-r from-[#000000] to-[#B3C0DF] text-transparent bg-clip-text">{t('title')}</h1>
+//             <p className="w-[600px] text-[24px] font-normal leading-[120%] tracking-[-2%] text-[#000000] opacity-80">
+//                 {t('description')}
+//             </p>
+//         </div>
+//         <Image className="mask-b-from-20% mask-b-to-110%" src="/images/macbookMockup1.png" alt="mackbook mockup 1" width={1021} height={662} />
+      
+//         <RadialBlurBg width={"150vw"} height={"150vh"} rotate={"-9.35deg"} top={"-50vh"} left={"-25vw"}/>
+//       </section>
+//     </div>
+//   );
+// }
