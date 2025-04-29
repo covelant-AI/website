998eca7de403b4d8edf5cfab697bcfbe9c16247b
AmNat789 <amit.amanna@code.berlin>
2025-04-29 22:04:16 +0200

    feat: add ContactSection to Approach page with localization support

--- app/[locale]/approach/page.tsx
+++ app/[locale]/approach/page.tsx
@@ -3,15 +3,23 @@ import Problems from "@components/approach/Problems";
 import InfiniteCarousel from "@components/InfiniteCarousel";
 import ScalingCards from "@components/ScalingCards";
 import Solution from "@components/approach/Solution";
+import ContactSection from "@components/ContactSection";
+import { useTranslations } from "next-intl";
 
 export default function Approach() {
+    const contact_t = useTranslations('components.approach.contact');
     return (
-        <div className="bg-[#F9F9F9] flex flex-col items-center justify-center z-1 gap-y-[80px] lg:gap-y-[160px] px-[10%] overflow-x-hidden">
+        <div className="bg-[#F9F9F9] flex flex-col items-center justify-center z-1 gap-y-[80px] lg:gap-y-[160px] px-[10%] overflow-hidden">
             <Intro />
             <Problems />
             <Solution/>
             <InfiniteCarousel/>
             <ScalingCards/>
+            <ContactSection 
+                title={contact_t('title')}
+                description={contact_t('description')}
+                image={"/images/footer-img-1.png"}
+            />
         </div>   
     )
 }
