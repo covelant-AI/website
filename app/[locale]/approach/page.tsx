deb9c64845ee8ae2bb6c2cc0c2fc912185b864e5
AmNat789 <amit.amanna@code.berlin>
2025-05-01 12:36:22 +0200

    refactor: enhance layout and responsiveness in Approach components

--- app/[locale]/approach/page.tsx
+++ app/[locale]/approach/page.tsx
@@ -9,7 +9,8 @@ import { useTranslations } from "next-intl";
 export default function Approach() {
     const contact_t = useTranslations('components.approach.contact');
     return (
-        <div className="bg-[#F9F9F9] flex flex-col items-center justify-center z-1 gap-y-[80px] lg:gap-y-[160px] px-[10%] overflow-hidden">
+        <>
+        <div className="bg-[#F9F9F9] flex flex-col z-1 gap-y-[80px] lg:gap-y-[160px] px-[10%] overflow-hidden pb-[80px]">
             <Intro />
             <Problems />
             <Solution/>
@@ -20,6 +21,10 @@ export default function Approach() {
                 description={contact_t('description')}
                 image={"/images/footer-img-1.png"}
             />
-        </div>   
+        </div>
+
+        {/* TODO: This is for the Footer Bg. Mke it similar to the index page */}
+        <div className="absolute h-[300px] w-full bg-[#F9F9F9]"/>
+        </>
     )
 }
