b211f1624cf7642759fd80c159a459c16afb81d9
Omar Badawy <omar.badawy@code.berlin>
2025-06-21 13:21:24 +0200

    fixed main page

--- components/HeroMain.jsx
+++ components/HeroMain.jsx
@@ -1,28 +1,46 @@
+"use client";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { motion } from "motion/react";
+import { AuroraBackground } from "@components/UI/AuroraBackground";
 import Link from "next/link";
-import { useTranslations } from "next-intl";
-
 
 export default function HeroMain() {
-  const t = useTranslations('components.heroMain');
   return (
-    <div className="relative mt-20 pt-10">
-      <section
-        className="font-Figtree flex flex-col items-center justify-left
-                   bg-[url('/images/heroBackground.png')] bg-cover bg-center bg-no-repeat after:absolute after:bottom-0 after:left-0 after:w-full after:h-1/3
-                   after:bg-gradient-to-b after:from-transparent after:to-[#F9F9F9] overflow-hidden">
-                    
-        <div className=" mx-20 my-60 text-left max-sm:mx-5 max-sm:my-40">
-          <h1 className="text-black font-semibold text-5xl mt-3 bg-gradient-to-r from-black to-gray-400 bg-clip-text text-transparent">{t("title")}</h1>
-          <p className="text-black font-normal text-xl mt-8">{t("subtitle")}</p>
-          <Link href="/contact">
-          <button className="mt-10 text-xl px-10 max-sm:px-8 max-sm:text-2xl py-2 bg-[#42B6B1] 
-            text-white font-normal font-Figtree rounded-2xl shadow-md hover:bg-teal-600">
-            Contact us
-          </button>
-          </Link>
-        </div>
-      </section>
-    </div>
+    <>
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="">
+        <section className="font-Figtree flex flex-col items-center justify-center overflow-hidden relative z-20">
+          <div className=" mx-20 my-60 text-left max-sm:mx-5 max-sm:my-40">
+            <h1 className="text-black font-semibold text-5xl mt-3 bg-gradient-to-r from-black to-gray-400 bg-clip-text text-transparent">
+              The world's first AI sports analytics tool</h1>
+            <p className="text-black font-normal text-xl mt-8">Elevate your game, trusted by coaches and players to boost performance, outsmart rivals, and win more.</p>
+            <Link href="/contact">
+            <button className="mt-10 text-xl px-10 max-sm:px-8 max-sm:text-2xl py-2 bg-[#42B6B1] 
+              text-white font-normal font-Figtree rounded-xl shadow-md hover:bg-teal-600">
+              Contact us
+            </button>
+            </Link>
+          </div>
+        </section>
+      </motion.div>
+    </AuroraBackground>
+      <div
+        className={cn(
+            "absolute inset-0",
+            "[background-size:100px_100px]",
+            "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)] z-1",
+        )}/>
+    </>
   );
 }
 
