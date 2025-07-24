5a4910ab0ece12f3a4d0877d449153cbd4be7fc4
Omar Badawy <omar.badawy@hotmail.com>
2025-07-24 18:28:26 +0200

    update

---
+++ prisma/migrations/20250724162754_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Inquiry" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "companyName" TEXT,
+    "inquiryType" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Inquiry_pkey" PRIMARY KEY ("id")
+);
